a139f5dcb79abad6d7de78ec3eb5a904ac421142
xilepeng <lepengxi@gmail.com>
2021-09-10 12:07:21 +0800

    slice

---
+++ code/slice/array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	Array1 := [5]int{}
+	Array2 := [2]int{}
+	fmt.Printf("Array1 的数据类型：%T\nArray2 的数据类型：%T\n", Array1, Array2)
+}
+
+/*
+Array1 的数据类型：[5]int
+Array2 的数据类型：[2]int
+*/
--- code/slice/slice2/main.go
+++ code/slice/slice2/main.go
@@ -4,21 +4,25 @@ import "fmt"
 
 func main() {
 	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	s1 := slice[2:5] //[2, 3, 4]
-	s2 := s1[2:6:7]  //[4, 5, 6, 7]
+	s1 := slice[2:5]
+	s2 := s1[2:6:7]
+	fmt.Printf("s1: len=%v, cap=%v\n", len(s1), cap(s1))
+	fmt.Printf("s2: len=%v, cap=%v\n", len(s2), cap(s2))
 
-	s2 = append(s2, 100) // s2 = [4, 5, 6, 7, 100]
-	s2 = append(s2, 200) // s2 = [4, 5, 6, 7, 100, 200]
+	s2 = append(s2, 100)
+	s2 = append(s2, 200)
+	fmt.Printf("\t扩容后 s2: len=%v, cap=%v\n", len(s2), cap(s2))
 
-	s1[2] = 20 //s1 = [2, 3, 20]
+	s1[2] = 20
 
 	fmt.Println(s1)
 	fmt.Println(s2)
 	fmt.Println(slice)
 }
 
-/*
-	[2 3 20]
-	[4 5 6 7 100 200]
-	[0 1 2 3 20 5 6 7 100 9]
-*/
+// s1: len=3, cap=8
+// s2: len=4, cap=5
+//         扩容后 s2: len=6, cap=10
+// [2 3 20]
+// [4 5 6 7 100 200]
+// [0 1 2 3 20 5 6 7 100 9]
