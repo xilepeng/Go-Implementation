cf157d42c7078ccca264aec9e2ae39ad407f0cfa
xilepeng <lepengxi@gmail.com>
2021-09-10 22:57:39 +0800

    slice

--- code/slice/slice1/main.go
+++ code/slice/slice1/main.go
@@ -7,7 +7,13 @@ import (
 func main() {
 	var nil_slice []int
 	new_slice := new([]int)
-	empty_slice := make([]int, 0) // []int{}
+
+	pointer_slice := new([]string)
+	*pointer_slice = append(*pointer_slice, "x")
+
+	fmt.Printf("*pointer_slice = %v\n\n ", *pointer_slice) // *pointer_slice = [x]
+
+	empty_slice := make([]int, 0) // empty_slice := []int{}
 	slice := []int{0, 1, 2}
 
 	fmt.Printf("nil_slice: %p, %v, %T \n\t array: %p, len: %v, cap: %v\n\n",
@@ -27,3 +33,15 @@ func main() {
 		&slice[0], len(slice), cap(slice))
 
 }
+
+// nil_slice: 0xc00000c030, [], []int
+// 	 array: 0x0, len: 0, cap: 0
+
+// new_slice: 0xc00000e028, &[], *[]int
+// 	 array: 0xc00000c048, len: 0, cap: 0
+
+// empty_slice: 0xc00000c060, [], []int
+// 	 array: 0x116ce80, len: 0, cap: 0
+
+// slice: 0xc00000c078, [0 1 2], []int
+// 	 array: 0xc00001e0d8, len: 3, cap: 3
